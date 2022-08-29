c875c0e6dd0a75766b25b05752e163377c0ce4b5
Andrei Raducanu <andrei.raducanu@sabs.ro>
2022-08-29 11:17:38 +0300

    feat(stock-page): add basic page layout

--- src/pages/StockPage.tsx
+++ src/pages/StockPage.tsx
@@ -1,3 +1,6 @@
+import { ReactNode } from 'react';
+import { Link } from 'react-router-dom';
+
 import useFetch from '@hooks/useFetch';
 import type { TimeSeriesDailyResponse } from 'types/TimeSeriesDailyResponse';
 
@@ -11,20 +14,52 @@ const StockPage = ({ symbol }: StockPageProps) => {
   );
 
   if (error) {
-    return <p>Ups... Something went wrong</p>;
+    return (
+      <PageLayout>
+        <p>Ups... Something went wrong</p>
+      </PageLayout>
+    );
   }
 
   if (!data) {
-    return <p>Loading...</p>;
+    return (
+      <PageLayout>
+        <p>Loading...</p>
+      </PageLayout>
+    );
   }
 
   if ('Error Message' in data) {
-    return <p>Invalid Symbol... Please try again</p>;
+    return (
+      <PageLayout>
+        <p>Invalid Symbol... Please try again</p>;
+      </PageLayout>
+    );
   }
 
   return (
-    <div>
+    <PageLayout>
       <pre>{JSON.stringify(data['Time Series (Daily)'], null, 2)}</pre>
+    </PageLayout>
+  );
+};
+
+const PageLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div>
+      <header className='flex items-center border border-b-gray-300 bg-white px-4 pt-4 pb-3'>
+        <Link className='flex h-full w-16 items-center justify-center' to='/'>
+          <img src='/duck.svg' className='h-10 w-10' />
+        </Link>
+        <div className='ml-4 h-11 w-[70%] max-w-lg rounded-xl border border-gray-200 bg-white shadow-sm focus-within:shadow-md hover:shadow-md'>
+          <input
+            className='h-full w-full border-0 bg-transparent p-0 pr-2 pl-4 text-gray-800 placeholder:text-gray-400 focus:ring-0'
+            type='text'
+            placeholder='Search...'
+          />
+        </div>
+      </header>
+      <main>{children}</main>
     </div>
   );
 };
